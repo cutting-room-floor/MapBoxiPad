b07e098172813e554059a46d675c98ce233f4c8d
Justin R. Miller <incanus@codesorcery.net>
2010-08-19 16:12:20 -0400

    added chalkboard & star field images & set star field to map background

--- Classes/MapBoxiPadDemoViewController.m
+++ Classes/MapBoxiPadDemoViewController.m
@@ -70,7 +70,7 @@ void SoundCompletionProc (SystemSoundID sound, void *clientData);
     mapView.enableRotate = NO;
     mapView.deceleration = YES;
 
-    mapView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"linen.jpg"]];
+    mapView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"starfield.jpg"]];
     
     // data overlay & layer managers
     //
