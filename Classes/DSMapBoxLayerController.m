fa80b6e1fb5d6383ef69b098fe2b4ef8afa719e6
Justin R. Miller <incanus@codesorcery.net>
2011-03-07 18:23:42 -0500

    fixes #31837: spinner during layer toggle operations

--- Classes/DSMapBoxLayerController.m
+++ Classes/DSMapBoxLayerController.m
@@ -16,6 +16,14 @@
 
 #import "Reachability.h"
 
+@interface DSMapBoxLayerController (DSMapBoxLayerControllerPrivate)
+
+- (void)toggleLayerAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
+
+#pragma mark -
+
 @implementation DSMapBoxLayerController
 
 @synthesize layerManager;
@@ -72,6 +80,49 @@
                                                                               action:@selector(tappedEditButton:)] autorelease];
     
     self.tableView.editing = NO;
+}
+
+#pragma mark -
+
+- (void)toggleLayerAtIndexPath:(NSIndexPath *)indexPath
+{
+    /**
+     * This routine takes care of the actual toggle, meant for use in a 
+     * performSelector:withObject:afterDelay: so as to animate properly 
+     * on the next run loop pass. 
+     *
+     * This assumes we're in selected, animating state, then performs
+     * the toggle, then when that returns, deselects the cell, removes 
+     * the animator accessory, and re-sets the cell selection state 
+     * properly.
+     */
+
+    [layerManager toggleLayerAtIndexPath:indexPath zoomingIfNecessary:YES];
+
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    [self.tableView cellForRowAtIndexPath:indexPath].accessoryView = nil;
+    
+    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    
+    NSArray *layers;
+    
+    switch (indexPath.section)
+    {
+        case 0:
+            layers = layerManager.baseLayers;
+            break;
+            
+        case 1:
+            layers = layerManager.tileLayers;
+            break;
+            
+        case 2:
+            layers = layerManager.dataLayers;
+            break;
+    }
+            
+    cell.accessoryType = ([[[layers objectAtIndex:indexPath.row] valueForKeyPath:@"selected"] boolValue] ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone);
 }
 
 #pragma mark -
@@ -204,12 +255,12 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
     if ([[tableView cellForRowAtIndexPath:indexPath].textLabel.text isEqual:kDSOpenStreetMapURL])
     {
         if ([[Reachability reachabilityForInternetConnection] currentReachabilityStatus] == NotReachable)
-        {        
+        {
+            [tableView deselectRowAtIndexPath:indexPath animated:NO];
+
             UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"No Internet Connection"
                                                              message:[NSString stringWithFormat:@"%@ tiles require an active internet connection.", kDSOpenStreetMapURL]
                                                             delegate:nil
@@ -222,9 +273,18 @@
         }
     }
     
-    [layerManager toggleLayerAtIndexPath:indexPath zoomingIfNecessary:YES];
-        
-    [self.tableView reloadData];
+    /**
+     * In response to potentially long toggle operations, we add a spinner 
+     * accessory view & start it animating. Then, we do the actual operation
+     * in the next run loop pass, which also takes care of removing the
+     * animation and re-setting selected state. 
+     */
+    
+    UIActivityIndicatorView *spinner = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite] autorelease];
+    [self.tableView cellForRowAtIndexPath:indexPath].accessoryView = spinner;
+    [spinner startAnimating];
+    
+    [self performSelector:@selector(toggleLayerAtIndexPath:) withObject:indexPath afterDelay:0.0];
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
