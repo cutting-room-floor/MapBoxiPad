58692734e52faad3fce30d96722fa0076357eb07
Justin R. Miller <incanus@codesorcery.net>
2012-01-05 10:58:35 -0800

    first cut of network activity indicator manager

---
+++ Classes/DSMapBoxNetworkActivityIndicator.h
@@ -0,0 +1,14 @@
+//
+//  DSMapBoxNetworkActivityIndicator.h
+//  MapBoxiPad
+//
+//  Created by Justin Miller on 1/5/12.
+//  Copyright (c) 2012 Development Seed. All rights reserved.
+//
+
+@interface DSMapBoxNetworkActivityIndicator : NSObject
+
++ (void)addJob:(id)item;
++ (void)removeJob:(id)item;
+
+@end
